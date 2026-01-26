25330c1139233636078fa1ea937b57032b7d2e6a
kzncod-e <kzncode@yahoo.com>
2026-01-26 13:50:22 +0700

    disabled some ununsed index

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["github.com"], // ← IZININ GITHUB DI SINI
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+    ],
   },
 };
 
---
+++ src/components/global/base-layout.tsx
@@ -0,0 +1,9 @@
+import React, { ReactHTMLElement } from "react";
+
+const BaseLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="container bg-background mx-auto  px-4">{children}</div>
+  );
+};
+
+export default BaseLayout;
