436a1eab301f9b286415f62ca916e832d7afcce7
theightdev <theightdev@gmail.com>
2026-01-29 14:15:36 +0700

    feat: handle error ui using custom component handler

--- src/modules/example-page/pages/500-page.tsx
+++ src/modules/example-page/pages/500-page.tsx
@@ -1,4 +1,4 @@
-import { Error500 } from "@/components/global/system/500";
+import { Error500 } from "@/components/global/system/error-500";
 
 export default function Error500Page() {
   return <Error500 errorId={`ERR-${Date.now()}`} />;
