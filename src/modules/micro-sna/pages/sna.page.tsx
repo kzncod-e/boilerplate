3c5c43ade2e45bbb50f27a443fb89c3460e34328
kzncod-e <kzncode@yahoo.com>
2026-02-03 16:45:01 +0700

    send theme as postmessage to the sna frame  to match the theme of the entire page

--- src/modules/micro-sna/pages/sna.page.tsx
+++ src/modules/micro-sna/pages/sna.page.tsx
@@ -1,7 +1,23 @@
+"use client";
+
 import PageHeader from "@/components/global/page-header";
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useTheme } from "next-themes";
 
 const MicroSNA = () => {
+  const { theme } = useTheme();
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  // Kirim theme ke iframe setiap kali berubah
+  useEffect(() => {
+    if (iframeRef.current && iframeRef.current.contentWindow) {
+      iframeRef.current.contentWindow.postMessage(
+        { type: "SET_THEME", theme },
+        "*",
+      );
+    }
+  }, [theme]);
+
   return (
     <>
       <PageHeader
@@ -10,8 +26,9 @@ const MicroSNA = () => {
       />
 
       <iframe
+        ref={iframeRef}
         src="/sna.html"
-        className="w-full h-[90vh]  border-none"
+        className="w-full h-[90vh] border-none"
         loading="lazy"
       />
     </>
